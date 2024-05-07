0cde6489b9086f8c768d21768b72559da7568d1a
mustaeen <136145181+must108@users.noreply.github.com>
2024-05-06 20:29:32 -0400

    reversed

--- src/types/index.ts
+++
@@ -1,9 +0,0 @@
-type Axis = 'x' | 'y' | 'z';
-type AxisValue = number;
-type Toward = 1 | -1;
-type Notation = string;
-type NotationBase = 'L' | 'M' | 'R' | 'D' | 'E' | 'U' | 'B' | 'S' | 'F';
-type NotationExtra = '' | `'` | '2'; 
-
-export type { Axis, AxisValue, Toward, Notation, NotationBase, NotationExtra }
-
